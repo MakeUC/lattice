557e4061a2566706b070397f5f8cff5e6f7ffd90
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-08-14 23:48:48 -0400

    Update background and spacing

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -7,25 +7,27 @@ import data from "../data.js";
 
 import "../styles/Deck.css";
 
-const to = i => ({
+const to = (i) => ({
   x: 0,
   y: i * -10,
   scale: 1,
-  rot: -10 + Math.random() * 20,
-  delay: i * 100
+  rot: -5 + Math.random() * 10,
+  delay: i * 100,
 });
-const from = i => ({ rot: 0, scale: 1.5, y: -1000 });
+
+const from = (i) => ({ rot: 0, scale: 1.5, y: -1000 });
 
 const trans = (r, s) =>
-  `perspective(1500px) rotateX(10deg) rotateY(${r /
-    10}deg) rotateZ(${r}deg) scale(${s})`;
+  `perspective(1500px) rotateX(10deg) rotateY(${
+    r / 10
+  }deg) rotateZ(${r}deg) scale(${s})`;
 
 function Deck() {
   const [gone] = useState(() => new Set());
 
-  const [props, set] = useSprings(data.length, i => ({
+  const [props, set] = useSprings(data.length, (i) => ({
     ...to(i),
-    from: from(i)
+    from: from(i),
   }));
 
   const bind = useGesture(
@@ -35,34 +37,37 @@ function Deck() {
       delta: [xDelta],
       distance,
       direction: [xDir],
-      velocity
+      velocity,
     }) => {
       const trigger = velocity > 0.2;
-
       const dir = xDir < 0 ? -1 : 1;
 
-      if (!down && trigger) gone.add(index);
+      if (!down && trigger) {
+        gone.add(index);
+      }
 
-      set(i => {
-        if (index !== i) return;
+      set((i) => {
+        if (index !== i) {
+          return;
+        }
+
         const isGone = gone.has(index);
-
         const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0;
-
         const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
-
         const scale = down ? 1.1 : 1;
+
         return {
           x,
           rot,
           scale,
           delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
+          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
         };
       });
 
-      if (!down && gone.size === data.length)
-        setTimeout(() => gone.clear() || set(i => to(i)), 600);
+      if (!down && gone.size === data.length) {
+        setTimeout(() => gone.clear() || set((i) => to(i)), 600);
+      }
     }
   );
 
