fa68dd6bd9d278eadb2ad934bcb8050e6d101f38
Dev Agrawal <devagrawal09@gmail.com>
2020-08-19 06:34:40 +0530

    created sample data

--- src/data.js
+++ src/data.js
@@ -1,30 +1,43 @@
+const js = {
+  title: "Javascript",
+  icon: "src-to-icon-image"
+};
+
+const web = {
+  title: "Web",
+  icon: "src-to-icon-image"
+};
+
+const python = {
+  title: "Python",
+  icon: "src-to-icon-image"
+};
+
+const cpp = {
+  title: "C++",
+  icon: "src-to-icon-image"
+};
+
+const java = {
+  title: "Java",
+  icon: "src-to-icon-image"
+};
+
+const typescript = {
+  title: "Typescript",
+  icon: "src-to-icon-image"
+};
+
+const net = {
+  title: ".NET",
+  icon: "src-to-icon-image"
+};
+
 export default [
   {
-    pics: [
-      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-    ],
-    name: "Placeholder",
-    age: 20,
-    distance: "Placeholder",
-    text: "Placeholder"
-  },
-  {
-    pics: [
-      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-    ],
-    name: "Placeholder",
-    age: 20,
-    distance: "Placeholder",
-    text: "Placeholder"
-  },
-  {
-    pics: [
-      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-    ],
-    name: "Placeholder",
-    age: 20,
-    distance: "Placeholder",
-    text: "Placeholder"
-  },
+    skills: [ js, web, java, typescript, python, net ],
+    name: "Dev Agrawal",
+    idea: "A tinder clone but for hackers and developers to meet people...",
+    lookingFor: [ cpp, java, web ]
+  }
 ];
