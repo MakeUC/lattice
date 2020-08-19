f8e76fc90a391461c55b5426f9f0570eaa227f5c
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-08-18 21:56:37 -0400

    Comment out invalid content

--- src/data.js
+++ src/data.js
@@ -1,43 +1,74 @@
-const js = {
-  title: "Javascript",
-  icon: "src-to-icon-image"
-};
+// const js = {
+//   title: "Javascript",
+//   icon: "src-to-icon-image"
+// };
 
-const web = {
-  title: "Web",
-  icon: "src-to-icon-image"
-};
+// const web = {
+//   title: "Web",
+//   icon: "src-to-icon-image"
+// };
 
-const python = {
-  title: "Python",
-  icon: "src-to-icon-image"
-};
+// const python = {
+//   title: "Python",
+//   icon: "src-to-icon-image"
+// };
 
-const cpp = {
-  title: "C++",
-  icon: "src-to-icon-image"
-};
+// const cpp = {
+//   title: "C++",
+//   icon: "src-to-icon-image"
+// };
 
-const java = {
-  title: "Java",
-  icon: "src-to-icon-image"
-};
+// const java = {
+//   title: "Java",
+//   icon: "src-to-icon-image"
+// };
 
-const typescript = {
-  title: "Typescript",
-  icon: "src-to-icon-image"
-};
+// const typescript = {
+//   title: "Typescript",
+//   icon: "src-to-icon-image"
+// };
 
-const net = {
-  title: ".NET",
-  icon: "src-to-icon-image"
-};
+// const net = {
+//   title: ".NET",
+//   icon: "src-to-icon-image"
+// };
+
+// export default [
+//   {
+//     skills: [ js, web, java, typescript, python, net ],
+//     name: "name",
+//     idea: "...",
+//     lookingFor: [ cpp, java, web ]
+//   }
+// ];
 
 export default [
   {
-    skills: [ js, web, java, typescript, python, net ],
-    name: "Dev Agrawal",
-    idea: "A tinder clone but for hackers and developers to meet people...",
-    lookingFor: [ cpp, java, web ]
-  }
+    pics: [
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    ],
+    name: "Placeholder",
+    age: 20,
+    distance: "Placeholder",
+    text: "Placeholder"
+  },
+  {
+    pics: [
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    ],
+    name: "Placeholder",
+    age: 20,
+    distance: "Placeholder",
+    text: "Placeholder"
+  },
+  {
+    pics: [
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    ],
+    name: "Placeholder",
+    age: 20,
+    distance: "Placeholder",
+    text: "Placeholder"
+  },
 ];
