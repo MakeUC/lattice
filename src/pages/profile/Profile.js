f8a5d33975adbec60c37c1cb68e4f9942d598872
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-08-28 06:39:09 -0400

    Improved Profile UI

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,47 +1,44 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import SlackIcon from '@material-ui/icons/AlternateEmail';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import { Button } from '@material-ui/core';
+import {Button, Container} from '@material-ui/core';
+
+import '../../styles/Profile.scss'
 
 const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1)
-  }
+    margin: {
+        margin: theme.spacing(1)
+    }
 }));
 
-export default function InputWithIcon() {
-  const classes = useStyles();
 
-  return (
-    <div>
-      <h1>Lattice Account</h1>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <MailOutlineIcon />
-          </Grid>
-          <Grid item>
-            <TextField
-              disabled
-              id="input-with-icon-grid"
-              label="user@makeuc.io"
-            />
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <SlackIcon />
-          </Grid>
-          <Grid item>
-            <TextField disabled id="input-with-icon-grid" label="username" />
-          </Grid>
-        </Grid>
-      </div>
-      <Button>Change Username</Button>
-      <Button>Change Password</Button>
-    </div>
-  );
+export default function InputWithIcon() {
+    const classes = useStyles();
+
+    return (
+        <Container>
+            <div className="bg-white mt5 pa3 pl5 pr5 register br3">
+                <h1 className="title">Lattice Account</h1>
+                <div className="mb4">
+                    <Grid container spacing={4} className="mb3 lattice-form-band" alignItems="flex-end">
+                        <Grid className="lattice-icon" item>
+                            <MailOutlineIcon/>
+                        </Grid>
+                        <p className="lattice-form-label mb0 font-gray">user@makeuc.io</p>
+                    </Grid>
+                    <Grid container spacing={4} className="mb3 lattice-form-band" alignItems="flex-end">
+                        <Grid item className="lattice-icon">
+                            <SlackIcon/>
+                        </Grid>
+                        <p className="lattice-form-label mb0 font-gray">MakeUC</p>
+                    </Grid>
+                </div>
+                <Button variant="contained" className="center profile-button" color="primary">Change Username</Button>
+                <Button variant="contained" className="center profile-button" color="primary">Change Password</Button>
+            </div>
+        </Container>
+    );
 }
