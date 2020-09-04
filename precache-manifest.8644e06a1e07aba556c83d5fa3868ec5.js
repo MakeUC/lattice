496aa959f3a9606e939c6f28e04797a161e6652a
Dev Agrawal <devagrawal09@gmail.com>
2020-09-04 13:56:50 +0530

    Updates

---
+++ precache-manifest.8644e06a1e07aba556c83d5fa3868ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69717cf26c0de39d8481f45972d08344",
+    "url": "/lattice-client/index.html"
+  },
+  {
+    "revision": "ae7ea7d51e0879658f4e",
+    "url": "/lattice-client/static/css/main.64583642.chunk.css"
+  },
+  {
+    "revision": "722fc6b93a5587787ba7",
+    "url": "/lattice-client/static/js/2.adccd0b4.chunk.js"
+  },
+  {
+    "revision": "359c58ea24d3f33c5ebdae66213ed364",
+    "url": "/lattice-client/static/js/2.adccd0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7ea7d51e0879658f4e",
+    "url": "/lattice-client/static/js/main.ea4d1ba1.chunk.js"
+  },
+  {
+    "revision": "d5eb61edf6bb6860a4d0",
+    "url": "/lattice-client/static/js/runtime-main.cd50be53.js"
+  }
+]);
