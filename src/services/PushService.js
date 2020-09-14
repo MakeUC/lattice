4ab9567639713f2916768f9c8770252ee6f406d9
Dev Agrawal <devagrawal09@gmail.com>
2020-09-14 20:40:53 +0530

    linting fix

--- src/services/PushService.js
+++ src/services/PushService.js
@@ -5,7 +5,7 @@ const apiUrl = `${apiHost}/notification`;
 
 const urlB64ToUint8Array = base64String => {
   const padding = `=`.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/\-/g, `+`).replace(/_/g, `/`);
+  const base64 = (base64String + padding).replace(/-/g, `+`).replace(/_/g, `/`);
   const rawData = atob(base64);
   const outputArray = new Uint8Array(rawData.length)
   for (let i = 0; i < rawData.length; ++i) {
