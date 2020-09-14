99db7fc8007a6ca6c80879a09f822877475ff518
Dev Agrawal <devagrawal09@gmail.com>
2020-09-14 21:16:12 +0530

    some debug logs

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -23,11 +23,13 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'development' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    console.log(`The URL constructor is available in all browsers that support SW`);
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      console.log(`Our service worker won't work if PUBLIC_URL is on a different origin from what our page is served on. This might happen if a CDN is used to serve assets`);
       return;
     }
 
@@ -36,6 +38,7 @@ export function register(config) {
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
+        console.log(`This is running on localhost. Let's check if a service worker still exists or not.`);
         checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
@@ -45,6 +48,7 @@ export function register(config) {
         });
       } else {
         // Is not localhost. Just register service worker
+        console.log('Is not localhost. Just register service worker');
         registerValidSW(swUrl, config);
       }
     });
@@ -52,9 +56,11 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log({ swUrl });
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
+      console.log({ registration });
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
