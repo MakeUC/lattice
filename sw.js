68be43b946dfff79b3728e9a7a3ff3e589a48f50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-20 20:25:13 +0000

    Deploy MakeUC/lattice to github.com/MakeUC/lattice.git:gh-pages

--- sw.js
+++ sw.js
@@ -43,3 +43,11 @@ const showLocalNotification = (title, body, swRegistration) => {
   };
   swRegistration.showNotification(title, options);
 };
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
