78766f1c4d1f0862ed78257db53d34f00e7baa34
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-09-29 20:30:18 -0400

    Fixed the wrong api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,6 @@
 const env = process.env.NODE_ENV;
 
 export const apiHost =
-  (env === `development`) ? `https://makeuc-registration-dev.herokuapp.com` :
-    `https://makeuc-registration-dev.herokuapp.com`;
+    (env === `development`) ?
+        `http://localhost:3000` :
+        `https://makeuc-registration.herokuapp.com`;
