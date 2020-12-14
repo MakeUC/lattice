dbcb970e5687a16e68f45632440545c39c1b3fa6
Dev Agrawal <devagrawal09@gmail.com>
2020-12-15 04:20:48 +0530

    using the heroku domain for ssl support

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -3,4 +3,4 @@ const env = process.env.NODE_ENV;
 export const apiHost =
     (env === `development`) ?
         `http://localhost:3000/api/v2/lattice` :
-        `http://api.revolutionuc.com/api/v2/lattice`;
+        `https://revolutionuc-api.herokuapp.com/api/v2/lattice`;
