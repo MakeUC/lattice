48442f7a681c9cc616a7d11a9c749600b5780250
Dev Agrawal <devagrawal09@gmail.com>
2021-07-04 17:27:11 -0400

    updated api host

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,5 +2,5 @@ const env = process.env.NODE_ENV;
 
 export const apiHost =
     (env === `development`) ?
-        `http://localhost:3000/api/v2/lattice` :
-        `https://revolutionuc-api.herokuapp.com/api/v2/lattice`;
+        `https://makeuc-registration-dev.herokuapp.com` :
+        `https://makeuc-registration-dev.herokuapp.com`;
