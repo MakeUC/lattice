9f9c271b604c39339a32a612188cdeba8e7ae1f7
Dev Agrawal <devagrawal09@gmail.com>
2021-09-13 21:12:33 -0400

    moving to prod api

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -3,4 +3,4 @@ const env = process.env.NODE_ENV;
 export const apiHost =
     (env === `development`) ?
         `http://localhost:4000` :
-        `https://makeuc-registration-dev.herokuapp.com`;
+        `https://makeuc-registration.herokuapp.com`;
