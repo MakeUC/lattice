29b08a41a59b6e67f6d8137e8689c6979f8a11b1
Arnav Komaragiri <komaraac@mail.uc.edu>
2021-09-21 19:53:49 -0400

    Update Api.ts

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,5 +2,5 @@ const env = process.env.NODE_ENV;
 
 export const apiHost =
     (env === `development`) ?
-        `http://localhost:4000` :
-        `https://makeuc-registration-dev.herokuapp.com`;
+        `https://makeuc-registration-dev.herokuapp.com` :
+        `https://makeuc-registration.herokuapp.com`;
